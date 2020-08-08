70e0fcc11f58459d4f3db8bea99a19a6b5c4dc6c
Sydney Keating <bookmasterdc@gmail.com>
2020-08-08 16:20:26 -0400

    Added camera shake and postprocessing

---
+++ CameraShake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    
+    void Start()
+    {
+        
+    }
+
+     private void Update() {
+         
+    }
+
+    public void shakecamera(bool shaking){
+        if(shaking){
+        this.GetComponent<Animator>().Play("CameraShakeAnim");
+        }
+        shaking =false;
+        
+    }
+   
+  
+}
+
